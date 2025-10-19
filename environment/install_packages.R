6130350128a056520c852920d49f7059ee5ddb51
CongWang3 <113320242+CongWang3@users.noreply.github.com>
2025-10-20 01:19:44 +0800

    Add files via upload
    directly upload

--- environment/install_packages.R
+++ environment/install_packages.R
@@ -1,12 +1,28 @@
 # Read package list
-packages <- read.csv("package_R.csv")
+# packages <- read.csv("package_R.csv")
 
+tryCatch({
+  packages <- read.csv("/tmp/package_R.csv", header = TRUE, fileEncoding="UTF-8")
+}, error = function(e) {
+
+  print(list.files("/tmp"))  # 打印目录内容
+  stop(e)
+})
+
+# 添加清华源
+options(repos = c(CRAN = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
 # Check and install BiocManager
 if (any(packages$Source == "Bioconductor")) {
   if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
   }
 }
+BiocManager::install(repos = "https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
+
+
+
+ 
+
 
 # Sub-source installation packages
 for (pkg in packages$Package) {
